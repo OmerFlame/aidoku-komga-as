0b12067949d5116bd267fe12b69668fe653e7977
Omer Shamai <renginald.bagel@gmail.com>
2022-08-01 15:44:53 +0300

    Fixed the chapter array to loop the correct amount of times

--- src/Komga.ts
+++ src/Komga.ts
@@ -210,7 +210,7 @@ export class Komga extends Source {
 
 		var chapterArray: Chapter[] = [];
 
-		for (let i = i32(array_len(contentArray.rid)); i >= 0; i--) {
+		for (let i = i32(array_len(contentArray.rid)) - 1; i >= 0; i--) {
 			let chapter = contentArray.asArray().get(i).asObject();
 			let chapterMeta = chapter.get("metadata").asObject();
 
